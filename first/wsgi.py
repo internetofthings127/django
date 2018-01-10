e04737e0863bd112d4ff08e1d879f325f89a6cdb
internetofthings127 <internetofthings127@outlook.com>
2018-01-10 13:07:06 +0400

    modify the letter

--- first/wsgi.py
+++ first/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for first project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposesaaaaa the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
